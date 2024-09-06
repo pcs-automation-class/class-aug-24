585d0c9a9b364e39af9bdcda6567aa5ba2ea26f4
Doronin-DI <Oranjeviyyashek@gmail.com>
2024-09-05 21:17:52 -0400

    Calculator test added

--- lesson_7_Dmitrii.py
+++ lesson_7_Dmitrii.py
@@ -1,3 +1,21 @@
-#version 09.04.2024 Dmitrii
-print('Hello world')
-print('it works')
+#version 09.05.2024 Dmitrii
+
+#block of inputs
+firstName =input('Enter your first name: ')
+lastName = input('Enter your last name: ')
+age = int(input('Enter your age: '))
+
+price = 10
+discountUnder16 = 0.1
+discountAbove65 = 0.9
+
+
+#block of calculating
+if age <= 10:
+    print('You are not allowed to buy this product.')
+elif age > 10 and age <= 16:
+    print('Dear ', firstName, lastName, 'your price is ',  10*(1-discountUnder16) )
+elif age > 16 and age <= 65:
+    print('Dear ', firstName, lastName, 'your price is ', 10 )
+elif age > 65:
+    print('Dear ', firstName, lastName, 'your price is ', 10*(1-discountAbove65) )
