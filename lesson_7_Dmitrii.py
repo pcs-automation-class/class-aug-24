08db99d11724be27a7efda0ecc7cf48641333b9b
Doronin-DI <Oranjeviyyashek@gmail.com>
2024-09-06 18:14:03 -0400

    new version of cod

--- lesson_7_Dmitrii.py
+++ lesson_7_Dmitrii.py
@@ -1,21 +1,25 @@
-#version 09.05.2024 Dmitrii
+#version 09.06.2024 Dmitrii
 
 #block of inputs
 firstName =input('Enter your first name: ')
 lastName = input('Enter your last name: ')
-age = int(input('Enter your age: '))
+age = int(input('Enter your age: ')  )
+
+def final_price(price, discount):
+    return price * (1-discount)
 
 price = 10
-discountUnder16 = 0.1
-discountAbove65 = 0.9
-
 
 #block of calculating
 if age <= 10:
     print('You are not allowed to buy this product.')
 elif age > 10 and age <= 16:
-    print('Dear ', firstName, lastName, 'your price is ',  10*(1-discountUnder16) )
+    discount=0.1
+    print('Dear ', firstName, lastName, 'your price is ',  final_price(price, discount) )
 elif age > 16 and age <= 65:
-    print('Dear ', firstName, lastName, 'your price is ', 10 )
+    print('Dear ', firstName, lastName, 'your price is ', price )
 elif age > 65:
-    print('Dear ', firstName, lastName, 'your price is ', 10*(1-discountAbove65) )
+    discount=0.9
+    print('Dear ', firstName, lastName, 'your price is ', round(final_price(price, discount), 2) )
+
+
